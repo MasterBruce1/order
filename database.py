82ce9be8c5dcff95b8f258985d26a4956529a222
MasterBruce1 <jx2512@columbia.edu>
2022-12-22 23:39:13 -0500

    updates for eb

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:dbuserdbuser@localhost:3306/orders"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://adminorder:dbuserbdbuser@dborder.cmm5ba669nkr.us-east-1.rds.amazonaws.com:3306/orders"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
